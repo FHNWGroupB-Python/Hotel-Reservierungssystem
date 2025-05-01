ffe5dde7f52beee63b41b8c5d3eb3955c1b1a0d5
Josip Jukic <josipjukic@macbook-air-von-josip.home>
2025-05-01 22:21:42 +0200

    Model:hotel.py ergänzt

---
+++ Model/hotel.py
@@ -0,0 +1,23 @@
+class Hotel:
+    def __init__(self, hotelid:int, hotel_name:str, street:str, city:str, zip_code:int, country:str, stars:int, number_of_rooms:int):
+        self.hotelid = hotelid
+        self.hotel_name = hotel_name
+        self.street = street
+        self.city = city
+        self.zip_code = zip_code
+        self.country = country
+        self.stars = stars
+        self.number_of_rooms = number_of_rooms
+    # Komposition (Das Zimmer gehört fest zu einem Hotel)
+        self.rooms = [
+            Room(
+                roomid = i,
+                room_number = i,
+                room_type = "",
+                price_per_night = 0.0,
+                hotel = self
+            )
+            for i in range(1, number_of_rooms + 1)
+        ]
+    # Assoziation: (Dienste können unabhängig existieren)
+        self.services = []
