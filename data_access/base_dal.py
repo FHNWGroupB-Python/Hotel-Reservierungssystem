ec7b2ed0a72937888feceb7fa079dbfcb2542b86
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-10 16:36:49 +0200

    Josip Files ergänzt

---
+++ data_access/base_dal.py
@@ -0,0 +1,52 @@
+import os
+import sqlite3
+
+
+class BaseDAL:
+    def __init__(self, db_connection:str = None):
+        if db_connection is None:
+            self.__db_connection_str = os.environ.get('DB_File')
+            if self.__db_connection_str is None:
+                raise Exception("DB_File environment variable not set")
+        else:
+            self.__db_connection_str = db_connection
+
+    def _connect(self):
+        return sqlite3.connect(self.__db_connection_str, detect_types=sqlite3.PARSE_DECLTYPES)
+
+    def fetchone(self, sql:str,  params:tuple = None):
+        if params is None:
+            params = ()
+        with self._connect() as conn:
+            try:
+                cursor = conn.execute(sql, params)
+                result = cursor.fetchone()
+            except sqlite3.Error as e:
+                conn.rollback()
+                raise e
+        return result
+
+    def fetchall(self, sql:str, params:tuple = None):
+        if params is None:
+            params = ()
+        with self._connect() as conn:
+            try:
+                cursor = conn.execute(sql, params)
+                results = cursor.fetchall()
+            except sqlite3.Error as e:
+                conn.rollback()
+                raise e
+        return results
+
+    def execute(self, sql:str, params:tuple = None):
+        if params is None:
+            params = ()
+        with self._connect() as conn:
+            try:
+                cursor = conn.execute(sql, params)
+            except sqlite3.Error as e:
+                conn.rollback()
+                raise e
+            else:
+                conn.commit()
+        return cursor.lastrowid, cursor.rowcount
