6a98494c3acaf78161e625e9bd02480f1746de6f
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-13 13:13:03 +0200

    Josip hat hotel_dal.py ergänzt

--- data_access/hotel_dal.py
+++ data_access/hotel_dal.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import model
 from data_access.base_dal import BaseDAL
 
-class HotelDAL(Base_DAL):
+class HotelDAL(BaseDAL):
     def __init__(self, db_path:str = None):
         super().__init__(db_path)
 
@@ -18,9 +18,29 @@ class HotelDAL(Base_DAL):
             zip_code,
             country,
             stars,
-            number_of_rooms,
-            if hotel else None,
+            number_of_rooms
         )
         last_row_id = self.execute_sql(sql, params)
         return model.Hotel(last_row_id, hotel_name, street, city, zip_code, country, stars, number_of_rooms)
+
+    def update_hotel(self, hotel: model.Hotel) -> None:
+        if hotel is None:
+            raise ValueError("Hotel cannot be None")
+
+        sql = """
+        UPDATE Hotel SET hotel_name = ?, street = ?, city = ?, zip_code = ?, country = ?, stars = ?, number_of_rooms = ? WHERE hotelid = ?
+        """
+        params = tuple([
+            hotel.hotel_name,
+            hotel.street,
+            hotel.city,
+            hotel.zip_code,
+            hotel.country,
+            hotel.stars,
+            hotel.number_of_rooms,
+            hotel.hotelid
+        ])
+
+        last_row_id, row_count = self.execute_sql(sql, params)
+
 
