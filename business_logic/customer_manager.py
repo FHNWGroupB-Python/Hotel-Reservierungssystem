a12371c1ca94e978198769947c48b295a5b54482
Ragujana <janarthanan.ragunathan@students.fhnw.ch>
2025-05-13 13:34:14 +0200

    janarthanan business Logic

---
+++ business_logic/customer_manager.py
@@ -0,0 +1,12 @@
+class CustomerManager:
+    def __init__(self):
+        self.__customer_da = data_access.CustomerDataAccess()
+
+    def create_customer(self, first_name: str, last_name: str, phone: str, email: str) -> model.Customer:
+        return self.__customer_da.create_customer(first_name, last_name, phone, email)
+
+    def update_customer(self, customer: model.Customer) -> None:
+        self.__customer_da.update_customer(customer)
+
+    def delete_customer(self, customer: model.Customer) -> None:
+        self.__customer_da.delete_customer(customer)
