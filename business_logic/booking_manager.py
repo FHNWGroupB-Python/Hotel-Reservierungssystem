7e086dd660535056b0c03efdf72ec3b128fd2ff4
Ragujana <janarthanan.ragunathan@students.fhnw.ch>
2025-05-13 13:32:55 +0200

    janarthanan business Logic

---
+++ business_logic/booking_manager.py
@@ -0,0 +1,15 @@
+class BookingManager:
+    def __init__(self):
+        self.__booking_da = data_access.BookingDataAccess()
+
+    def create_booking(self, customer: model.Customer, hotel: model.Hotel, number_of_guests: int, check_in: str, check_out: str) -> model.Booking:
+        return self.__booking_da.create_booking(customer, hotel, number_of_guests, check_in, check_out)
+
+    def update_booking(self, booking: model.Booking) -> None:
+        self.__booking_da.update_booking(booking)
+
+    def cancel_booking(self, booking: model.Booking) -> None:
+        self.__booking_da.cancel_booking(booking)
+
+    def show_booking_details(self, booking: model.Booking) -> str:
+        return booking.get_details()
