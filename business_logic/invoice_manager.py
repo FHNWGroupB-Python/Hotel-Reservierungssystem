c57238664fe02a6e637551c29e67ebe64742eb65
Ragujana <janarthanan.ragunathan@students.fhnw.ch>
2025-05-13 13:36:02 +0200

    janarthanan business Logic

---
+++ business_logic/invoice_manager.py
@@ -0,0 +1,12 @@
+class InvoiceManager:
+    def __init__(self):
+        self.__invoice_da = data_access.InvoiceDataAccess()
+
+    def create_invoice(self, amount: float, invoice_date: str) -> model.Invoice:
+        return self.__invoice_da.create_invoice(amount, "offen", invoice_date)
+
+    def delete_invoice(self, invoice: model.Invoice) -> None:
+        self.__invoice_da.delete_invoice(invoice)
+
+    def show_invoice_details(self, invoice: model.Invoice) -> str:
+        return invoice.get_details()
