29aa625e37ce7870f552fcad0e4de0cbbc46b555
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-18 13:11:45 +0200

    Payment method ergänzt

--- model/payment_method.py
+++ model/payment_method.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-
-    from model.booking import Booking
 
 class PaymentMethod:
     def __init__ (self, paymentid: int, additional_service: str):
-        self.paymentid = paymentid
-        self.additional_service = additional_service
+        self.__paymentid = paymentid
+        self.__additional_service = additional_service
 
-    def __str__(self):
-        return (f"Rechnungs ID: {self.paymentid}, Additional Service: {self.additional_service}")
+    @property
+    def paymentid(self):
+        return self.__paymentid
+
+    @property
+    def additional_service(self):
+        return self.__additional_service
