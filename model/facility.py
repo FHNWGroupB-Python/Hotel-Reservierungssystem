3c648190304d2fe86239cfec32ea49a4d2a014c6
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-18 13:21:13 +0200

    Facility.py angepasst

--- model/facility.py
+++ model/facility.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-
-    from model.hotel import Hotel
 
 class Facility:
     def __init__(self, facilityid: int, facility: str):
-        self.facilityid = facilityid
-        self.facility = facility
+        self.__facilityid = facilityid
+        self.__facility = facility
 
-    def __str__(self):
-        return (f"Einrichtung: {self.facility}")
+    @property
+    def facilityid(self):
+        return self.__facilityid
+
+    @property
+    def facility(self):
+        return self.__facility
