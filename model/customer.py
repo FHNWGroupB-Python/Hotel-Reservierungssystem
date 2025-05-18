4953535a6913c6268dcfedabbc74760cc0a2177f
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-18 13:24:28 +0200

    customer.py angepasst

--- model/customer.py
+++ model/customer.py
@@ -6,12 +6,48 @@ if TYPE_CHECKING:
 
 class Customer:
     def __init__(self, customerid:int, first_name:str, last_name:str, phone:str, email:str):
-        self.customerid = customerid
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone = phone
-        self.email = email
+        self.__customerid = customerid
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__phone = phone
+        self.__email = email
         self.bookings = [] # Assoziation (Kunde in Verbindung 1-zu-n zu Buchung)
 
-    def __str__(self):
-        return (f"Kunden ID: {self.customerid}, first_name: {self.first_name}, last_name: {self.last_name}, phone: {self.phone}, email: {self.email}")
+    def add_booking(self, booking: "Booking"):
+        self.bookings.append(booking)
+
+    @property
+    def customerid(self):
+        return self.__customerid
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self.__first_name = first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self.__last_name = last_name
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        self.__phone = phone
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
