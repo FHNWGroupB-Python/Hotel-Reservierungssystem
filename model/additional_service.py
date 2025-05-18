e999e2a6436a06f5882739d5984258adde5ceb70
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-18 13:50:59 +0200

    additional service angepasst

--- model/additional_service.py
+++ model/additional_service.py
@@ -2,9 +2,23 @@ from __future__ import annotations
 
 class AdditionalService:
     def __init__(self, additionalserviceid, additional_service, additional_service_price):
-        self.additionalserviceid = additionalserviceid
-        self.additional_service = additional_service
-        self.additional_service_price = additional_service_price
+        self.__additionalserviceid = additionalserviceid
+        self.__additional_service = additional_service
+        self.__additional_service_price = additional_service_price
 
-    def __str__(self):
-        return (', '.join(f'{key}: {value}' for key, value in vars(self).items()))
+    @property
+    def additionalserviceid(self):
+        return self.__additionalserviceid
+
+    @property
+    def additional_service(self):
+        return self.__additional_service
+
+    @property
+    def additional_service_price(self):
+        return self.__additional_service_price
+
+    @additional_service.setter
+    def additional_service(self, additional_service):
+        self.__additional_service = additional_service
+
