4cc4320b69bfe9ed97cfe46647520566dd2b3d4a
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-22 16:09:44 +0200

    Josip files angepasst

--- business_logic/room_type_manager.py
+++ business_logic/room_type_manager.py
@@ -5,3 +5,11 @@ class RoomTypeManager:
     def __init__(self):
         self.__room_type_dal = data_access.RoomTypeDAL(db_path = "database/")
 
+    def create_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        return self.__room_type_dal.create_room_type(room_type)
+
+    def update_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        return self.__room_type_dal.update_room_type(room_type)
+
+    def delete_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        return self.__room_type_dal.delete_room_type(room_type)
--- data_access/room_type_dal.py
+++ data_access/room_type_dal.py
@@ -1,8 +1,40 @@
 from __future__ import annotations
 
+import model
 from data_access.base_dal import BaseDAL
 
 class RoomTypeDAL(BaseDAL):
     def __init__(self, db_path:str = None):
         super().__init__(db_path)
+
+    def create_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        sql = """
+        INSERT INTO RoomType (description, max_guests) VALUES (?, ?)
+        """
+        params = (
+            room_type.description,
+            room_type.max_guests
+        )
+        self.execute(sql, params)
+
+    def update_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        sql = """
+        UPDATE RoomType SET description = ?, max_guest = ? WHERE room_type_id = ?
+        """
+        params = tuple([
+            room_type.description,
+            room_type.max_guests
+        ])
+        self.execute(sql, params)
+
+    def delete_room_type(self, room_type: model.RoomType) -> model.RoomType:
+        sql = """
+        DELETE FROM RoomType WHERE room_type_id = ?
+        """
+        params = (
+            room_type.room_type_id,
+        )
+        self.execute(sql, params)
+
+
 
