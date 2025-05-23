844056f608dcd6a670237c1d4a137fead6d6569d
Josip Jukic <josipjukic@MacBook-Air-von-Josip.local>
2025-05-23 11:48:08 +0200

    Facility dal angepasst

--- data_access/facility_dal.py
+++ data_access/facility_dal.py
@@ -1,7 +1,35 @@
 from __future__ import annotations
 
+import model
 from data_access.base_dal import BaseDAL
 
 class FacilityDAL(BaseDAL):
     def __init__(self, db_path:str = None):
         super().__init__(db_path)
+
+    def create_facility(self, facility: model.Facility) -> model.Facility:
+        sql = """
+        INSERT INTO Facility (facility_name) VALUES (?)
+        """
+        params = (
+            facility.facility_name,
+        )
+        self.execute(sql, params)
+
+    def update_facility(self, facility: model.Facility) -> model.Facility:
+        sql = """
+        UPDATE Facility SET facility_name = ? WHERE facility_id = ?
+        """
+        params = (
+            facility.facility_name,
+        )
+        self.execute(sql, params)
+
+    def delete_facility(self, facility: model.Facility) -> model.Facility:
+        sql = """
+        DELETE FROM Facility WHERE facility_id = ?
+        """
+        params = (
+            facility.facility_id,
+        )
+        self.execute(sql, params)
