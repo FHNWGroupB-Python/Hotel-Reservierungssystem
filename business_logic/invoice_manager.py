9c9f37ed4bd96feca0463da5b719bd99a3996f61
Ragujana <janarthanan.ragunathan@students.fhnw.ch>
2025-05-29 19:14:37 +0200

    janarthanan

--- business_logic/invoice_manager.py
+++ business_logic/invoice_manager.py
@@ -8,3 +8,7 @@ class InvoiceManager:
     def create_invoice(self, issue_date: str, total_amount: float) -> model.Invoice:
         self.__invoice_dal.create_invoice(issue_date, total_amount)
 
+    def read_invoice(self) -> list[model.Invoice]:
+        return self.__invoice_dal.read_invoice()
+
+
