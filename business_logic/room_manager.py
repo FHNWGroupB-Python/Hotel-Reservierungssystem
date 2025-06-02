ad1a6ca6b8d5432f1cd71508ae1b49b84ef9cd1c
JJukic <josip.jukic@windowslive.com>
2025-06-02 13:29:53 +0200

    Josip Userstory 2.2 hinzugefügt

--- business_logic/room_manager.py
+++ business_logic/room_manager.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import model
 import data_access
 
@@ -16,3 +18,8 @@ class RoomManager:
 
     def get_room_info_by_hotel(self, name: str) -> list[model.Room]:
         return self.__room_dal.get_room_info_by_hotel(name) # TODO Logik implementieren für try/except
+
+    def get_available_rooms_by_date(self, check_in_date: date, check_out_date: date) -> list[model.Room]:
+        return self.__room_dal.get_available_rooms_by_date(check_in_date, check_out_date) # TODO Logik für Datum ergänzen damit zuerst geprüft wird welches Zimmer frei ist
+
+
