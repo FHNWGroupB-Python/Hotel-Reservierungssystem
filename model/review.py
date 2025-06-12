632dc91ba9fa7cf0e32bb7a6098f8a4b011fb631
JJukic <josip.jukic@windowslive.com>
2025-06-12 17:39:38 +0200

    modul review angepasst.

--- model/review.py
+++ model/review.py
@@ -9,12 +9,24 @@ if TYPE_CHECKING:
 class Review:
     def __init__(self, review_id:   int, hotel: Hotel, guest: Guest, rating: int, comment: str, review_date: date):
         self.__review_id = review_id
-        self.hotel = hotel
-        self.guest = guest
-        self.rating = rating
-        self.comment = comment
-        self.review_date = review_date
+        self.__hotel = hotel
+        self.__guest = guest
+        self.__rating = rating
+        self.__comment = comment
+        self.__review_date = review_date
 
     @property
     def review_id(self) -> int:
         return self.__review_id
+
+    @property
+    def rating(self) -> int:
+        return self.__rating
+
+    @property
+    def comment(self) -> str:
+        return self.__comment
+
+    @property
+    def review_date(self) -> date:
+        return self.__review_date
